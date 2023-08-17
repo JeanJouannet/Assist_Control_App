c1537472844c6e0d340db9c898c0e3533a98cfcb
JeanJouannet <jeanjouannet98@gmail.com>
2023-08-17 10:53:06 -0400

    making packages

---
+++ src/main/java/com/rule1/assist_control_app/AssistControlAppApplication.java
@@ -0,0 +1,13 @@
+package com.rule1.assist_control_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistControlAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistControlAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rule1/assist_control_app/AssistControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rule1.assist_control_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistControlAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
